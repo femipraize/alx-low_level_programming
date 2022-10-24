259ddb200ca16bcc119f4f44eab72a1899dc0794
femipraize <felixogbangwor@mail.com>
2022-10-24 23:33:10 +0000

    Error fixes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,16 @@
 #include <unistd.h>
 
 /**
- * main - prints a text in C
+ * main - prints 
  *
  * Retrun: Always 1 (Successful)
- *
  */
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2,
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+		59);
+
 	return (1);
 }
