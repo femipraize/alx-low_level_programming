801837e2fbc87ab562e6ec25c396564448da3035
femipraize <felixogbangwor@gmail.com>
2022-10-29 02:27:13 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - prints straight line n times.
+ * @n: times straight line is printed.
+ * Return: no return.
+ */
+void print_line(int n)
+{
+	int i = 0
+		while (i < n && n > 0)
+		{
+			_putchar('-');
+			i++;
+		}
+	_putchar('\n');
+}
